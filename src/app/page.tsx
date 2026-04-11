ad959b602ca2d36b158fdeb36568052a8573426e
hepad123 <heffy29@hotmail.com>
2026-04-11 02:28:12 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -177,8 +177,8 @@ export default function HomePage() {
         setSearchOpen(false);
       }
     }
-    document.addEventListener("mousedown", handleClick);
-    return () => document.removeEventListener("mousedown", handleClick);
+    document.addEventListener("mouseup", handleClick);
+return () => document.removeEventListener("mouseup", handleClick);
   }, []);
 
   const revealBase: React.CSSProperties = {
